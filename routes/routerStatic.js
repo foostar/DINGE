ebb497be8d4bb7a496a96f8795a49a1f4a66e019
xiusiteng <xiusiteng@goyoo.com>
2016-06-02 16:59:38 +0800

    同步豆瓣数据demo页面

--- routes/routerStatic.js
+++ routes/routerStatic.js
@@ -2,9 +2,7 @@
  * Created by Administrator on 2016-06-01.
  * 用户端静态路由
  */
-var send=require('send')
+var send = require("send")
 module.exports=function(app){
-    app.get('/',function(req,res){
-         send(req, path.join(__dirname, 'public/index.html')).pipe(res)
-    })
+    
 }
