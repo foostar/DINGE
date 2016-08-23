8840e97bc8ff19d145da3119e0d2e17485156dcd
pangxiaoqing <xiaoqing246810@163.com>
2016-08-23 17:40:05 +0800

    发现页面掉接口

--- public/js/home.js
+++ public/js/home.js
@@ -83,49 +83,47 @@ $(function(){
         url:"../data/getCommentsByRight.json",
         type:"GET",
         data:{},
-        datatype:"json",
-        success:function(res){
-            //console.log(res.data[0].commentFrom.nickname);
-            $(res.data).each(function(i,ele){
-                var monent = ele.createdAt;
-                var monsubstr=monent.substr(0, 10);
-                //获取当前时间，格式YYYY-MM-DD
-                function getNowFormatDate() {
-                    var date = new Date();
-                    var seperator1 = "-";
-                    var year = date.getFullYear();
-                    var month = date.getMonth() + 1;
-                    var strDate = date.getDate();
-                    if (month >= 1 && month <= 9) {
-                        month = "0" + month;
-                    }
-                    if (strDate >= 0 && strDate <= 9) {
-                        strDate = "0" + strDate;
-                    }
-                    var currentdate = year + seperator1 + month + seperator1 + strDate;
-                    //return currentdate;
-                    if(monsubstr.substr(0,4)==currentdate.substr(0,4) && monsubstr.substr(5,2)==currentdate.substr(5,2)){
-                        if(parseInt(monsubstr.substr(8,2))+1==parseInt(currentdate.substr(8,2))){
-                            $("#createdAt").text("昨天");
-                        }else if(parseInt(monsubstr.substr(8,2))+2==parseInt(currentdate.substr(8,2))){
-                            $("#createdAt").text("两天前");
-                        }else{
-                            $("#createdAt").text(monsubstr);
-                        }
+        datatype:"json"
+    }).done(function(res){
+        //console.log(res.data[0].commentFrom.nickname);
+        $(res.data).each(function(i,ele){
+            var monent = ele.createdAt;
+            var monsubstr=monent.substr(0, 10);
+            //获取当前时间，格式YYYY-MM-DD
+            function getNowFormatDate() {
+                var date = new Date();
+                var seperator1 = "-";
+                var year = date.getFullYear();
+                var month = date.getMonth() + 1;
+                var strDate = date.getDate();
+                if (month >= 1 && month <= 9) {
+                    month = "0" + month;
+                }
+                if (strDate >= 0 && strDate <= 9) {
+                    strDate = "0" + strDate;
+                }
+                var currentdate = year + seperator1 + month + seperator1 + strDate;
+                //return currentdate;
+                if(monsubstr.substr(0,4)==currentdate.substr(0,4) && monsubstr.substr(5,2)==currentdate.substr(5,2)){
+                    if(parseInt(monsubstr.substr(8,2))+1==parseInt(currentdate.substr(8,2))){
+                        $("#createdAt").text("昨天");
+                    }else if(parseInt(monsubstr.substr(8,2))+2==parseInt(currentdate.substr(8,2))){
+                        $("#createdAt").text("两天前");
                     }else{
                         $("#createdAt").text(monsubstr);
                     }
+                }else{
+                    $("#createdAt").text(monsubstr);
                 }
-                getNowFormatDate();
-                $("#nickname").text(ele.commentFrom.nickname);
-                $("#nickname").css("backgroundImage","url(.."+ele.commentFrom.avatar+")");
-                $("#Title").text(ele.title);
-                $("#home_img").attr("src",ele.movie.images.small);
-                $("#reading").text(ele.reading);
-                $("#Rank").text(ele.rank);
-                $("#star").text(ele.star);
-            });
-        }
+            }
+            getNowFormatDate();
+            $("#nickname").text(ele.commentFrom.nickname);
+            $("#nickname").css("backgroundImage","url(.."+ele.commentFrom.avatar+")");
+            $("#Title").text(ele.title);
+            $("#home_img").attr("src",ele.movie.images.small);
+            $("#reading").text(ele.reading);
+            $("#Rank").text(ele.rank);
+            $("#star").text(ele.star);
+        });
     });
-
 });
--- public/js/search.js
+++ public/js/search.js
@@ -1,5 +1,43 @@
 var $ = window.jQuery;
+
 $(function(){
-    // 加载底部
+	// 加载底部
     $("#footer").load("../views/footer.html");
+    //电影列表的接口
+    $.ajax({
+        url:"../data/showMovieList.json",
+        method:"GET",
+        data:{
+            token:$.cookie("dinge")
+        },
+        dataType:"json"
+    }).done(function(result){
+        var html = "";
+        if(result.status == 1 && result.data.length>0){ 
+            var data = result.data;
+            data.forEach(function(item){
+               //console.log(item.images.medium);
+
+                html += "<li><img src="+item.images.large+" alt=''><span>"+item.title+"</span></li>";
+            });
+            $(html).appendTo($(".search_body"));
+        }
+    });
+    /*.done(function(result){
+        var html = "";
+        if(result.status == 1 && result.data.length>0){
+            var data = result.data;
+            //console.log(data);
+            for(var i=0;i<data.length;i++){
+                //console.log(data[ i ].title);
+                html += "<li><img src="+data[ i ].images.large+" alt=''><span>"+data[ i ].title+"</span></li>";
+            }
+            $(html).appendTo($(".search_body"));
+        }
+    });*/
+    //点击搜索跳转搜索Ajax.load
+    $("#sousuo").change(function(){
+        //$(document).load(searchMovie.html);
+        window.location.href = "searchMovie.html";
+    });
 });
