734a2801d1114fbc81a6b1ff6804e20cc4a1edcb
pangxiaoqing <xiaoqing246810@163.com>
2016-09-30 10:33:54 +0800

    修改home页面的js函数式编程

--- public/js/home.js
+++ public/js/home.js
@@ -44,91 +44,115 @@ $(function(){
 
         });
     });*/
+    function Home(){
+        
+    }
+    Home.prototype = {
+        init:function(){
+            this.bindEvent();
+            this.render();
+        },
+        bindEvent:function(){
+            this.startStep1();
+        },
+        startStep1:function(){
+            lunboTu();
+            comment();
+        }
+    };
+    var home = new Home();
+    home.init();
+
+    function lunboTu(){
+        $.ajax({
+            url:"../data/getCarousels.json",
+            method:"GET",
+            /*data:{
+                token:$.cookie("dinge")
+            },*/
+            dataType:"json",
+            beforeSend:function(){
+                $(".loading").show();
+            }
+        }).done(function(result){
+            $(".loading").hide();
+            var html = "";
+            if(result.status == 1 && result.data.length>0){ 
+                var data = result.data;
+                data.forEach(function(item){
+                   //console.log(item.url);
+
+                    html += "<div class='swiper-slide'><a class='pic' href='javascript://'><img src="+item.url+" alt=''/></a></div>";
+                });
+                $(html).appendTo($(".swiper-wrapper"));
+            }
+        }).done(function(result){
+            if(result.status == 1){
+                new Swiper(".swiper-container",{  
+                    direction:"horizontal",//横向滑动  
+                    loop:true,//形成环路（即：可以从最后一张图跳转到第一张图 
+                    pagination:".swiper-pagination",//分页器  
+                    prevButton:".swiper-button-prev",//前进按钮 
+                    nextButton:".swiper-button-next",//后退按钮 
+                    autoplay:3000//每隔3秒自动播放
+
+                });
+            }
+        });
+    }
+
     
-    $.ajax({
-        url:"../data/getCarousels.json",
-        method:"GET",
-        /*data:{
-            token:$.cookie("dinge")
-        },*/
-        dataType:"json",
-        beforeSend:function(){
-            $(".loading").show();
-        }
-    }).done(function(result){
-        $(".loading").hide();
-        var html = "";
-        if(result.status == 1 && result.data.length>0){ 
-            var data = result.data;
-            data.forEach(function(item){
-               //console.log(item.url);
-
-                html += "<div class='swiper-slide'><a class='pic' href='javascript://'><img src="+item.url+" alt=''/></a></div>";
-            });
-            $(html).appendTo($(".swiper-wrapper"));
-        }
-    }).done(function(result){
-        if(result.status == 1){
-            new Swiper(".swiper-container",{  
-                direction:"horizontal",//横向滑动  
-                loop:true,//形成环路（即：可以从最后一张图跳转到第一张图 
-                pagination:".swiper-pagination",//分页器  
-                prevButton:".swiper-button-prev",//前进按钮 
-                nextButton:".swiper-button-next",//后退按钮 
-                autoplay:3000//每隔3秒自动播放
-
-            });
-        }
-    });
 
 
 
 /*---------------------------------------home page 首页评论 start--------------------------------*/
-    $.ajax({
-        url:"../data/getCommentsByRight.json",
-        type:"GET",
-        data:{},
-        datatype:"json"
-    }).done(function(res){
-        //console.log(res.data[0].commentFrom.nickname);
-        $(res.data).each(function(i,ele){
-            var monent = ele.createdAt;
-            var monsubstr=monent.substr(0, 10);
-            //获取当前时间，格式YYYY-MM-DD
-            function getNowFormatDate() {
-                var date = new Date();
-                var seperator1 = "-";
-                var year = date.getFullYear();
-                var month = date.getMonth() + 1;
-                var strDate = date.getDate();
-                if (month >= 1 && month <= 9) {
-                    month = "0" + month;
-                }
-                if (strDate >= 0 && strDate <= 9) {
-                    strDate = "0" + strDate;
-                }
-                var currentdate = year + seperator1 + month + seperator1 + strDate;
-                //return currentdate;
-                if(monsubstr.substr(0,4)==currentdate.substr(0,4) && monsubstr.substr(5,2)==currentdate.substr(5,2)){
-                    if(parseInt(monsubstr.substr(8,2))+1==parseInt(currentdate.substr(8,2))){
-                        $("#createdAt").text("昨天");
-                    }else if(parseInt(monsubstr.substr(8,2))+2==parseInt(currentdate.substr(8,2))){
-                        $("#createdAt").text("两天前");
+    function comment(){
+        $.ajax({
+            url:"../data/getCommentsByRight.json",
+            type:"GET",
+            data:{},
+            datatype:"json"
+        }).done(function(res){
+            //console.log(res.data[0].commentFrom.nickname);
+            $(res.data).each(function(i,ele){
+                var monent = ele.createdAt;
+                var monsubstr=monent.substr(0, 10);
+                //获取当前时间，格式YYYY-MM-DD
+                function getNowFormatDate() {
+                    var date = new Date();
+                    var seperator1 = "-";
+                    var year = date.getFullYear();
+                    var month = date.getMonth() + 1;
+                    var strDate = date.getDate();
+                    if (month >= 1 && month <= 9) {
+                        month = "0" + month;
+                    }
+                    if (strDate >= 0 && strDate <= 9) {
+                        strDate = "0" + strDate;
+                    }
+                    var currentdate = year + seperator1 + month + seperator1 + strDate;
+                    //return currentdate;
+                    if(monsubstr.substr(0,4)==currentdate.substr(0,4) && monsubstr.substr(5,2)==currentdate.substr(5,2)){
+                        if(parseInt(monsubstr.substr(8,2))+1==parseInt(currentdate.substr(8,2))){
+                            $("#createdAt").text("昨天");
+                        }else if(parseInt(monsubstr.substr(8,2))+2==parseInt(currentdate.substr(8,2))){
+                            $("#createdAt").text("两天前");
+                        }else{
+                            $("#createdAt").text(monsubstr);
+                        }
                     }else{
                         $("#createdAt").text(monsubstr);
                     }
-                }else{
-                    $("#createdAt").text(monsubstr);
                 }
-            }
-            getNowFormatDate();
-            $("#nickname").text(ele.commentFrom.nickname);
-            $("#nickname").css("backgroundImage","url(.."+ele.commentFrom.avatar+")");
-            $("#Title").text(ele.title);
-            $("#home_img").attr("src",ele.movie.images.small);
-            $("#reading").text(ele.reading);
-            $("#Rank").text(ele.rank);
-            $("#star").text(ele.star);
+                getNowFormatDate();
+                $("#nickname").text(ele.commentFrom.nickname);
+                $("#nickname").css("backgroundImage","url(.."+ele.commentFrom.avatar+")");
+                $("#Title").text(ele.title);
+                $("#home_img").attr("src",ele.movie.images.small);
+                $("#reading").text(ele.reading);
+                $("#Rank").text(ele.rank);
+                $("#star").text(ele.star);
+            });
         });
-    });
+    }
 });
