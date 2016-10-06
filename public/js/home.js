4975251036075107c695b2ec544a52cd2d126e12
pangxiaoqing <xiaoqing246810@163.com>
2016-10-06 20:51:20 +0800

    修改home页面和search页面

--- public/js/home.js
+++ public/js/home.js
@@ -2,9 +2,6 @@ var $ = window.jQuery,
         Swiper = window.Swiper,
         dingeTools = window.dingeTools;
 $(function(){
-    // 加载底部
-    $("#footer").load("../views/footer.html");
-    /*---------------------home page 轮播图接口------------------------------*/
     /*$.ajax({
         url:"../data/getCarousels.json",
         method:"GET",
@@ -47,130 +44,104 @@ $(function(){
     });*/
 
     function Home(){
-        
+        this.init();
     }
     Home.prototype = {
         init:function(){
             dingeTools.init();
             // loading
-            this.bindEvent();
+            //$(".loading").hide();
+            //this.bindEvent();
             this.render();
 
         },
-        bindEvent:function(){
-            this.startStep1();
+        loadingFooter:function(){
+            // 加载底部  
+            $("#footer").load("../views/footer.html");
+        },
+        loadingImg:function(){
+            $(".loading").show();
         },
         loadingBanner:function(){
+            //轮播图部分
+            var dtd = $.Deferred();
+            $.ajax({
+                url:"../data/getCarousels.json",
+                method:"GET",
+                dataType:"json",
+            }).done(function(result){
+                var html = "";
+                if(result.status == 1 && result.data.length>0){ 
+                    var data = result.data;
+                    data.forEach(function(item){
+                       //console.log(item.url);
+
+                        html += "<div class='swiper-slide'><a class='pic' href='javascript://'><img src="+item.url+" alt=''/></a></div>";
+                    });
+                    $(html).appendTo($(".swiper-wrapper"));
+                }
+            }).done(function(result){
+                if(result.status == 1){
+                    new Swiper(".swiper-container",{  
+                        direction:"horizontal",//横向滑动  
+                        loop:true,//形成环路（即：可以从最后一张图跳转到第一张图 
+                        pagination:".swiper-pagination",//分页器  
+                        prevButton:".swiper-button-prev",//前进按钮 
+                        nextButton:".swiper-button-next",//后退按钮 
+                        autoplay:3000//每隔3秒自动播放
+
+                    });
+                }
+            });
+            dtd.resolve();
         },
         loadingComent:function(){
-
+            var dtd = $.Deferred();
+            $.ajax({
+                url:"../data/getCommentsByRight.json",
+                type:"GET",
+                data:{},
+                datatype:"json"
+            }).done(function(res){
+                var data = res.data,
+                    html = "";
+                console.log(data);
+                data.forEach(function(item,index){                   
+                    html += "<div class='home_comment_block flex-container";
+                    if(index%2){
+                    html+=" home_comment_right";
+                    }
+                    html +=  "'>"
+                                +"<img class='home_comment_img flex-normal' src='"+item.movie.images.small+"' alt=''>"
+                                +"<div class='home_comment flex-normal'>"
+                                    +"<div class='home_comment_author font-normal'>"
+                                        +"<em>"+dingeTools.fromNow(item.createdAt)+"</em>"
+                                        +"<a href='other_means.html'><span style='background-image:url(../.."+item.commentFrom.avatar+");'>"+item.commentFrom.nickname+"</span></a>"
+                                    +"</div>"
+                                    +"<div class='home_comment_title'>"+item.content+"</div>"
+                                    +"<div class='flex-container home_comment_meta font-normal'>"
+                                        +"<div class='flex-normal'>阅读<span>"+item.reading+"</span></div>"
+                                        +"<div class='flex-normal'>评论<span>"+item.rank+"</span></div>"
+                                        +"<div class='flex-normal'>喜欢<span>"+item.weight+"</span></div>"
+                                    +"</div>"
+                                +"</div>"
+                            +"</div>";
+                });
+                $(html).appendTo($("#loadingComent"));       
+            });
+            dtd.resolve();
         },
+        // 渲染页面
         render:function(){
-            
-            
+            var self = this;
+            this.loadingFooter();
+            this.loadingImg();
             $.when(this.loadingBanner(),this.loadingComent())
             .then(function(result){
-
+                $(".loading").hide();   
             })
         },
-        startStep1:function(){
-            lunboTu();
-            //comment();
-        }
     };
     var home = new Home();
-    home.init();
-
-    function lunboTu(){
-        $.ajax({
-            url:"../data/getCarousels.json",
-            method:"GET",
-            /*data:{
-                token:$.cookie("dinge")
-            },*/
-            dataType:"json",
-            beforeSend:function(){
-                $(".loading").show();
-            }
-        }).done(function(result){
-            $(".loading").hide();
-            var html = "";
-            if(result.status == 1 && result.data.length>0){ 
-                var data = result.data;
-                data.forEach(function(item){
-                   //console.log(item.url);
-
-                    html += "<div class='swiper-slide'><a class='pic' href='javascript://'><img src="+item.url+" alt=''/></a></div>";
-                });
-                $(html).appendTo($(".swiper-wrapper"));
-            }
-        }).done(function(result){
-            if(result.status == 1){
-                new Swiper(".swiper-container",{  
-                    direction:"horizontal",//横向滑动  
-                    loop:true,//形成环路（即：可以从最后一张图跳转到第一张图 
-                    pagination:".swiper-pagination",//分页器  
-                    prevButton:".swiper-button-prev",//前进按钮 
-                    nextButton:".swiper-button-next",//后退按钮 
-                    autoplay:3000//每隔3秒自动播放
-
-                });
-            }
-        });
-    }
-
-    
-
-
-
-/*---------------------------------------home page 首页评论 start--------------------------------*/
-    function comment(){
-        $.ajax({
-            url:"../data/getCommentsByRight.json",
-            type:"GET",
-            data:{},
-            datatype:"json"
-        }).done(function(res){
-            //console.log(res.data[0].commentFrom.nickname);
-            $(res.data).each(function(i,ele){
-                var monent = ele.createdAt;
-                var monsubstr=monent.substr(0, 10);
-                //获取当前时间，格式YYYY-MM-DD
-                function getNowFormatDate() {
-                    var date = new Date();
-                    var seperator1 = "-";
-                    var year = date.getFullYear();
-                    var month = date.getMonth() + 1;
-                    var strDate = date.getDate();
-                    if (month >= 1 && month <= 9) {
-                        month = "0" + month;
-                    }
-                    if (strDate >= 0 && strDate <= 9) {
-                        strDate = "0" + strDate;
-                    }
-                    var currentdate = year + seperator1 + month + seperator1 + strDate;
-                    //return currentdate;
-                    if(monsubstr.substr(0,4)==currentdate.substr(0,4) && monsubstr.substr(5,2)==currentdate.substr(5,2)){
-                        if(parseInt(monsubstr.substr(8,2))+1==parseInt(currentdate.substr(8,2))){
-                            $("#createdAt").text("昨天");
-                        }else if(parseInt(monsubstr.substr(8,2))+2==parseInt(currentdate.substr(8,2))){
-                            $("#createdAt").text("两天前");
-                        }else{
-                            $("#createdAt").text(monsubstr);
-                        }
-                    }else{
-                        $("#createdAt").text(monsubstr);
-                    }
-                }
-                getNowFormatDate();
-                $("#nickname").text(ele.commentFrom.nickname);
-                $("#nickname").css("backgroundImage","url(.."+ele.commentFrom.avatar+")");
-                $("#Title").text(ele.title);
-                $("#home_img").attr("src",ele.movie.images.small);
-                $("#reading").text(ele.reading);
-                $("#Rank").text(ele.rank);
-                $("#star").text(ele.star);
-            });
-        });
-    }
+   // home.init();
 });
--- public/js/search.js
+++ public/js/search.js
@@ -1,43 +1,63 @@
 var $ = window.jQuery;
 
 $(function(){
-	// 加载底部
-    $("#footer").load("../views/footer.html");
-    //电影列表的接口
-    $.ajax({
-        url:"../data/showMovieList.json",
-        method:"GET",
-        data:{
-            token:$.cookie("dinge")
+    function Search(){
+        this.init();
+    }
+    Search.prototype={
+        init:function(){
+            dingeTools.init();
+            this.render();
         },
-        dataType:"json"
-    }).done(function(result){
-        var html = "";
-        if(result.status == 1 && result.data.length>0){ 
-            var data = result.data;
-            data.forEach(function(item){
-               //console.log(item.images.medium);
+        loadingFooter:function(){
+            // 加载底部  
+            $("#footer").load("../views/footer.html");
+        },
+        loadingImg:function(){
+            $(".loading").show();
+        },
+        movieList:function(){
+            //电影列表的接口
+            var dtd = $.Deferred();
+            $.ajax({
+                url:"../data/showMovieList.json",
+                method:"GET",
+                data:{
+                    token:$.cookie("dinge")
+                },
+                dataType:"json"
+            }).done(function(result){
+                var html = "";
+                if(result.status == 1 && result.data.length>0){ 
+                    var data = result.data;
+                    data.forEach(function(item){
+                       //console.log(item.images.medium);
 
-                html += "<li><img src="+item.images.large+" alt=''><span>"+item.title+"</span></li>";
+                        html += "<li><img src="+item.images.large+" alt=''><span>"+item.title+"</span></li>";
+                    });
+                    $(html).appendTo($(".search_body"));
+                }
             });
-            $(html).appendTo($(".search_body"));
-        }
-    });
-    /*.done(function(result){
-        var html = "";
-        if(result.status == 1 && result.data.length>0){
-            var data = result.data;
-            //console.log(data);
-            for(var i=0;i<data.length;i++){
-                //console.log(data[ i ].title);
-                html += "<li><img src="+data[ i ].images.large+" alt=''><span>"+data[ i ].title+"</span></li>";
-            }
-            $(html).appendTo($(".search_body"));
-        }
-    });*/
-    //点击搜索跳转搜索Ajax.load
-    $("#sousuo").change(function(){
-        //$(document).load(searchMovie.html);
-        window.location.href = "searchMovie.html";
-    });
+            dtd.resolve();
+        },
+        changeHref:function(){
+            //点击搜索跳转搜索Ajax.load
+            $("#sousuo").change(function(){
+                //$(document).load(searchMovie.html);
+                window.location.href = "searchMovie.html";
+            });
+        },
+        // 渲染页面
+        render:function(){
+            var self = this;
+            this.loadingFooter();
+            this.loadingImg();
+            self.changeHref();
+            $.when(this.movieList())
+            .then(function(result){
+                $(".loading").hide();   
+            })
+        },
+    };
+    var search = new Search();
 });
